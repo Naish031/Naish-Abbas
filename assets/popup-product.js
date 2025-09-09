9d1f86044504146df38a0cc2b9a30f04ccd53d36
Naish Abbas <naish031@gmail.com>
2025-09-09 18:59:54 +0500

    fix: all selects dropdown working for all popups

--- assets/popup-product.js
+++ assets/popup-product.js
@@ -37,4 +37,56 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
   });
+
+  // Select Down Logic
+  const sizeSelectTriggerWrappers = document.querySelectorAll(
+    '.popup__size-trigger-wrapper'
+  );
+  if (!sizeSelectTriggerWrappers) return;
+
+  sizeSelectTriggerWrappers.forEach((wrapper) => {
+    const sizeSelectOptions = wrapper.querySelector('.popup__size-options');
+
+    const sizeOptions = wrapper.querySelectorAll('.popup__size-option');
+
+    const arrow = wrapper.querySelector('.icon-arrow-down');
+    const triggerLabel = wrapper.querySelector('.popup__size-trigger');
+
+    // enhance accessibility
+    wrapper.setAttribute('role', 'button');
+    wrapper.setAttribute('aria-haspopup', 'listbox');
+    wrapper.setAttribute('aria-expanded', 'false');
+
+    wrapper.addEventListener('click', function (event) {
+      event.stopPropagation();
+
+      const isOpen = sizeSelectOptions.classList.toggle('is-open');
+
+      if (arrow) arrow.classList.toggle('rotate-180', isOpen);
+    });
+
+    sizeOptions.forEach((option) => {
+      option.addEventListener('click', function (e) {
+        e.stopPropagation();
+        triggerLabel.textContent = option.textContent;
+        triggerLabel.classList.add('selected');
+
+        sizeSelectOptions.classList.remove('is-open');
+        if (arrow) arrow.classList.remove('rotate-180');
+      });
+    });
+  });
+
+  // Close the size select if clicking outside of it
+  window.addEventListener('click', function () {
+    sizeSelectTriggerWrappers.forEach((wrapper) => {
+      const sizeSelectOptions = wrapper.querySelector('.popup__size-options');
+      const arrow = wrapper.querySelector('.icon-arrow-down');
+
+      if (sizeSelectOptions.classList.contains('is-open')) {
+        sizeSelectOptions.classList.remove('is-open');
+        if (arrow) arrow.classList.remove('rotate-180');
+      }
+    });
+  });
 });
